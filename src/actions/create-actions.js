4086e40931647c55786382c2815bc033e6adf3a2
charlene bastians <charlenebastians@hotmail.com>
2018-09-27 14:41:44 +0100

    test and action for create new

--- src/actions/create-actions.js
+++ src/actions/create-actions.js
@@ -1,9 +1,20 @@
 export const OPEN_NEW_NOTE = "OPEN_NEW_NOTE";
+export const CREATE_NEW = "CREATE_NEW";
 
 export const open_New_Note = () => {
   return {
     type: OPEN_NEW_NOTE,
     creating: true,
     completed: false
+  };
+};
+
+export const create_New = (newTitle, newBody) => {
+  return {
+    type: CREATE_NEW,
+    title: newTitle,
+    body: newBody,
+    completed: true,
+    creating: false
   };
 };
--- src/test/create-actions.test.js
+++ src/test/create-actions.test.js
@@ -1,13 +1,32 @@
 import * as createActions from "../actions/create-actions";
 
+const newTitle = "Hello";
+const newBody = "testing123";
+
 describe("create actions", () => {
   describe("open_New_Note", () => {
-    it("should open up a new note cor creation", () => {
+    it("should open up a new note for creation", () => {
       const expectedAction = {
         type: createActions.OPEN_NEW_NOTE,
         creating: true,
         completed: false
       };
+      expect(createActions.open_New_Note()).toEqual(expectedAction);
+    });
+  });
+
+  describe("create_New", () => {
+    it("should save a new note", () => {
+      const expectedAction = {
+        type: createActions.CREATE_NEW,
+        title: newTitle,
+        body: newBody,
+        completed: true,
+        creating: false
+      };
+      expect(createActions.create_New(newTitle, newBody)).toEqual(
+        expectedAction
+      );
     });
   });
 });
