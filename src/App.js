31f9564e7bffb12344dfd343df6185160bab12d4
charlene bastians <charlenebastians@hotmail.com>
2018-10-03 16:00:54 +0100

    home page has create todo link, show todos rerenders home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import CreateTodo from "./containers/create-todo-container";
+import { Link, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CreateTodo />
+        <Link to="/">Create Todo</Link>
+        <Route exact={true} path="/" component={CreateTodo} />
       </div>
     );
   }
